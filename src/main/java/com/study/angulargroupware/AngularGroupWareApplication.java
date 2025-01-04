9272573d8d7168b25aba832d8ebae39feeb1d52e
고용수 <ajemxkem@gmail.com>
2025-01-05 04:09:03 +0900

    Initial commit

---
+++ src/main/java/com/study/angulargroupware/AngularGroupWareApplication.java
@@ -0,0 +1,13 @@
+package com.study.angulargroupware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularGroupWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AngularGroupWareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/angulargroupware/AngularGroupWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.angulargroupware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularGroupWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
